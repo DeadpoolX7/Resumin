fcad241409c143211016ddb566cd2a8b42ae2c8a
Sanju Shaw <shawsanju411@gmail.com>
2024-07-31 11:41:31 +0530

    footer style changed a bit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 const Footer = () => {
   return (
     <footer className='footer footer-center  text-base-content rounded p-10 text-3xl antialiased bg-slate-800'>
-        <div className='flex'>
+        <div className='flex flex-col lg:flex-row'>
             Build by <a href="http://x.com/sanju_shaw49496?t=s6Wgu4JAIQTMIIhpeAJkJg&s=09" target="_blank" rel="noopener noreferrer" className='hover:underline '>@sanju_shaw49496</a>
         </div>
     </footer>
