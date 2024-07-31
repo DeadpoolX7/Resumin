aac9c4675f0443d1654e7ac85a1432c501e04b6f
Sanju Shaw <shawsanju411@gmail.com>
2024-07-31 11:25:12 +0530

    localhost set

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://resumin-stef.onrender.com',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       }
